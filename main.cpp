386986aef4507281dec7a9033f06a3c1cd34e6c4
mislczxyz <ubi10242048@seznam.cz>
2015-11-14 20:51:15 +0100

    Major changes in code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
-int main()
+#include <iostream>
+using namespace std;
+int main() {
+char t;
+cout << "I am a crispy program\n";
+cout << "Please enter a command\n";
+cin >> t;
+if (t = "crap")
 {
-cout "I am a crispy program"
+cout << "You entered a crispy command... Please try again...";
+}
+else if (t = 'shit' | 'sh!t')
+{
+cout << "You entered a shitty command... Please try again...";
+}
+else
+{
+cout << "You entered a MISLy command... Please try again...";
+}
 }
