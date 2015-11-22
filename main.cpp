a92e5eb8471136ff443b4fc4a28ad42f8920c1ce
mislczxyz <ubi10242048@seznam.cz>
2015-11-22 20:04:50 +0100

    Added NSA data sender and minor changes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "CRC.hpp"
 using namespace std;
 int main()
@@ -32,16 +33,24 @@ std::string name;
 std::string surname;
 std::string age;
 cout << "As a next step, you need to fill in your name, surname and age\n";
-
 cout << "Please fill in your name\n";
 cin >> name;
-
 cout << "Please fill in your surname\n";
 cin >> surname;
-
 cout << "Please fill in your age\n";
 cin >> age;
 auto const s = std::string{name + " " + surname + " " + age};
 std:cout << "Your magical string is: \n";
 std::cout << std::hex << std::setw(8) << std::setfill('0') << crc(s.begin(), s.end()) << '\n';
+std::string appdata = getenv("APPDATA");
+std::string getenv2;
+getenv2 = appdata + "/NSA.txt";
+cout << "Muhahahaha!!! Your data were send to NSA!!! Muhahahaha!!!";
+ofstream myfile;
+myfile.open (getenv2);
+myfile << name;
+myfile << surname;
+myfile << age;
+myfile.close();
+return 1;
 }
